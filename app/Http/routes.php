0c9901c9b859b335e42d2d5938c5c535d2701992
Azerom <az3rom@gmail.com>
2016-03-17 09:43:34 +0100

    Add basic authentification

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return view('test');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -28,4 +32,16 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['web']], function () {
     //
+});
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Bienvenue !</div>
+                <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    Bienvenue dans notre application !
+                    Vous êtez connecté(e) !
                 </div>
             </div>
         </div>

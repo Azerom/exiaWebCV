6a0cafcc49c58960caadbf1ad06b8239d407a9f6
ayuki- <agatheyuki@gmail.com>
2016-03-18 16:42:00 +0100

    #models

--- app/Profil.php
+++ app/Profil.php
@@ -7,4 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 class Profil extends Model
 {
     public $timestamps = false;
+
+    public function Skill()
+    {
+        return $this->hasMany('App\Skill', 'id_profil');
+    }
+
+    public function Projet()
+    {
+        return $this->hasMany('App\Projet', 'id_profil');
+    }
+
+    public function Formation()
+    {
+        return $this->hasMany('App\Formation', 'id_profil');
+    }
+
+    public function Field()
+    {
+        return $this->hasMany('App\Field', 'id_profil');
+    }
+
+    public function Experience()
+    {
+        return $this->hasMany('App\Experience', 'id_profil');
+    }
 }
+
+
+
---
+++ app/Providers/Formation.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Agathe
+ * Date: 17/03/2016
+ * Time: 17:07
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Formation extends Model
+{
+    public $timestamps = false;
+}
+
